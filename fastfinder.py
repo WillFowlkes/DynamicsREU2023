ebfe483224dbf6794d9206b0762d47bf8b0a81ba
Will Fowlkes <willfowlkes@Wills-MacBookPro.local>
2023-07-11 14:29:42 -0400

    horray

--- fastfinder.py
+++ fastfinder.py
@@ -32,11 +32,10 @@ def initialize(d):
 def calc_resource_vector(d, a_vector):
     time = d["Tmax"]
     resource = [0 for _ in range(time)]
-    resource[0] = d["r"] / a_vector[0]
     for i in range(1, time):
-        if a_vector[i] != 0:
-            resource[i] += resource[i - 1] + d["r"] / a_vector[i]
-    for i in range(time):
+        if a_vector[i-1] != 0:
+            resource[i] += (resource[i - 1] + d["r"] / a_vector[i-1])
+    for i in range( time):
         resource[i] += d["Rmin"]
     return resource
 
@@ -62,7 +61,7 @@ def get_Rmax_index(d, resource):
     for i, j in enumerate(resource):
         if j >= d["Rmax"]:
             return i
-
+    return len(resource)
 
 def update_matrix(date, i, timing_matrix, a_vector):
     for j, __ in enumerate(a_vector):
@@ -80,24 +79,28 @@ def get_departure_vector(d, a_vector):
         for i, _ in enumerate(departure_vector):
             if val > i:
                 departure_vector[i] += 1
+            if departure_vector[i] == d["Tmax"]:
+                departure_vector[i] = "n"
     return departure_vector
 
 def get_payoffs(d, departure_vector, a_vector):
     resource = calc_resource_vector(d, a_vector)
     payoff = [0]*d["n"]
     for player, time in enumerate(departure_vector):
-        payoff[player] = calc_payoff(d, time, resource[time])
+        if time != "n":
+            payoff[player] = calc_payoff(d, time, resource[time])
+        else:
+            r = get_Rmax_index(d, resource)
+            payoff[player] = calc_nodisperse_payoff(d,r)
     return payoff
 
-def main():
-    # parameters = parameter.Parameter()
-    # parameters.setAll()
-    # parameters.set("b")
-    # print(parameters.get("b"))
-    #
-    # print("printing results...")
+def calc_nodisperse_payoff(d, day):
+    return (((d["Tmax"] - day ) / d["Tmax"]) * (d["f"]) + (
+                                                        d["N"] - 1) * (d["f"]))
 
-    d = {"N": 2,
+def test_cases():
+    print("Test case 1")
+    case1 = {"N": 2,
          "n": 3,
          "r": 9,
          "c": 6,
@@ -107,20 +110,99 @@ def main():
          "b": 1,
          "k": 1,
          "f": 10}
-    # set conditions for root node
-    rootInfo = {
-        'states': ['n'] * d['n'],
-        'efrs': [0] * d['n'],
-        'day': 0,
-        'p': 0,
-        'resources': [d['Rmin']] * d['n']
-    }
-    
+    a1 = a_finder(case1)
+    dept1 = get_departure_vector(case1, a1)
+    if dept1 == [1, 0, 0]:
+        print("PASS: Departure date")
+    else:
+        print("FAIL: Departure date: expected [1,0,0], got " + str(dept1))
+    payoffs1 = get_payoffs(case1, dept1, a1)
+    if payoffs1 == [33/2, 99/7, 99/7]:
+        print("PASS: Payoffs")
+    else:
+        print("FAIL: Payoffs: expected ["+str(33/2)+", " +str(99/7)+", "+ str(99/7)+"], got "+str(
+            payoffs1))
 
-    a_vector = a_finder(d)
-    departure_vector = get_departure_vector(d, a_vector)
-    print(get_payoffs(d, departure_vector, a_vector))
-    print(departure_vector)
+    print("Test case 2")
+    case2 = {"N": 10,
+             "n": 2,
+             "r": 4,
+             "c": 3,
+             "Rmin": 4,
+             "Rmax": 10,
+             "Tmax": 3,
+             "b": 1,
+             "k": 2,
+             "f": 5}
+    a2 = a_finder(case2)
+    dept2 = get_departure_vector(case2, a2)
+    if dept2 == ["n", "n"]:
+        print("PASS: Departure date")
+    else:
+        print("FAIL: Departure date: expected [n, n], got " + str(dept2))
+    payoffs2 = get_payoffs(case2, dept2, a2)
+    if payoffs2 == [45, 45]:
+        print("PASS: Payoffs")
+    else:
+        print("FAIL: Payoffs: expected [45, 45], got " + str(payoffs2))
+
+    print("Test case 3")
+    case3 = {"N": 1,
+             "n": 4,
+             "r": 20,
+             "c": 10,
+             "Rmin": 20,
+             "Rmax": 30,
+             "Tmax": 2,
+             "b": 2,
+             "k": 10,
+             "f": 10}
+    a3 = a_finder(case3)
+    dept3 = get_departure_vector(case3, a3)
+    if dept3 == [1, 1, 0, 0]:
+        print("PASS: Departure date")
+    else:
+        print("FAIL: Departure date: expected [1,1,0,0], got " + str(dept3))
+    payoffs3 = get_payoffs(case3, dept3, a3)
+    if payoffs3 == [6,6,4,4]:
+        print("PASS: Payoffs")
+    else:
+        print("FAIL: Payoffs: expected [6,6,4,4], got " + str(payoffs3))
+
+    print("Test case 4")
+    case4 = {"N": 2,
+             "n": 2,
+             "r": 6,
+             "c": 3,
+             "Rmin": 12,
+             "Rmax": 24,
+             "Tmax": 4,
+             "b": 2,
+             "k": 6,
+             "f": 6}
+    a4 = a_finder(case4)
+    dept4 = get_departure_vector(case4, a4)
+    if dept4 == [3, 3]:
+        print("PASS: Departure date")
+    else:
+        print("FAIL: Departure date: expected [3, 3], got " + str(dept4))
+    payoffs4 = get_payoffs(case4, dept4, a4)
+    if payoffs4 == [56/9, 56/9]:
+        print("PASS: Payoffs")
+    else:
+        print("FAIL: Payoffs: expected [56/9, 56/9], got " + str(payoffs4))
+
+
+def main():
+    # parameters = parameter.Parameter()
+    # parameters.setAll()
+    # parameters.set("b")
+    # print(parameters.get("b"))
+    #
+    # print("printing results...")
+
+
+    test_cases()
 
 
 if __name__ == '__main__':
